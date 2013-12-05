def8f3665502493dc14ca1d0dc3c059946d83bad
Parnell Springmeyer <parnell@ixmat.us>
2013-12-05 15:59:00 -0600

    Bumping minor version fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup, find_packages
 
-version = '0.5.6'
+version = '0.5.7'
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.md')) as f:
